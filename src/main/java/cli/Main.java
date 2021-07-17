7d4b68a4bf357db0d929316f0162625e3d37f2ae
Benjamin Weber <beni.weber27@outlook.com>
2021-07-17 23:09:18 +0200

    update

--- src/main/java/cli/Main.java
+++ src/main/java/cli/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         final var sql = PostgresConnectionFactory.build();
         final var studentRepository = new PostgreSqlStudentRepository(sql);
+        // CLI = Command Line Interface
         final var application = new CliApplication(studentRepository);
 
         application.run();
